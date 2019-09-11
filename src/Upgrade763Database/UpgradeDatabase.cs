8b4025129620f5e7be8c32d497ee666f1d455d6d
Zoltán Gyebrovszki <kavics@sensenet.com>
2019-09-11 02:39:00 +0200

    Initial

---
+++ src/Upgrade763Database/UpgradeDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SenseNet.Packaging;
+using SenseNet.Packaging.Steps;
+
+namespace Upgrade763Database
+{
+    public class UpgradeDatabase : Step
+    {
+        public override void Execute(ExecutionContext context)
+        {
+            //ExecuteScript("scripts/");
+        }
+    }
+}

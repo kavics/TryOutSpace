f67489a571637651b8fa198435f56c550cce4eb2
kavics <gyeby5@gmail.com>
2019-10-05 03:49:38 +0200

    Required permissions demo. (#4)

---
+++ src/Authorization/Framework/PermissionEntry.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace Framework
+{
+    public class PermissionEntry
+    {
+        public string Role { get; set; }
+        public PermissionType[] Permissions { get; private set; } = new PermissionType[0];
+
+        /// <summary>
+        /// Adds a permission if there is not yet
+        /// </summary>
+        internal void AddPermission(PermissionType permission)
+        {
+            Permissions = Permissions.Union(new[] { permission }).Distinct().ToArray();
+        }
+        /// <summary>
+        /// Removes a permission if there is.
+        /// Returns true if the entry has become empty.
+        /// </summary>
+        internal bool RemovePermission(PermissionType permission)
+        {
+            Permissions = Permissions.Except(new[] { permission }).ToArray();
+            return !Permissions.Any();
+        }
+    }
+}
